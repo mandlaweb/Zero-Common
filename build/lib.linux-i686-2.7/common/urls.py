8bb203592355ce69b66a6c352cd0c70232a62b6e
Jose Maria Zambrana Arze <contact@josezambrana.com>
2012-03-15 17:59:29 -0400

    [build] removed

--- build/lib.linux-i686-2.7/common/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
-
-from common.views import FlashView
-
-
-urlpatterns = patterns('',
-    url('^error$', FlashView.as_view(), name='error')
-)
