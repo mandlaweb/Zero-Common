e786ab7835d9d35aa2c2e06a28bc51505d75fa3a
Jose Maria Zambrana Arze <contact@josezambrana.com>
2012-09-21 10:24:12 -0400

    changes on setup

--- setup.py
+++ setup.py
@@ -1,40 +1,29 @@
-import os
-import logging
+from os.path import abspath, dirname, join, normpath
 
-from distutils.command.install import INSTALL_SCHEMES
-from distutils.core import setup
-
-
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('common'):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from setuptools import find_packages, setup
 
 
 setup(
-    name='zero-common',
-    version='0.1',
-    description='App with common views, libs and templates that can be used by other apps',
-    author='Jose Maria Zambrana Arze',
-    author_email='contact@josezambrana.com',
-    url='http://github.com/mandlaweb/zerocommon',
-    packages=packages,
-    data_files=data_files,
-    install_requires=['Django>=1.3.1', 'South>=0.7.3']
+
+    # Basic package information:
+    name = 'zero-common',
+    version = '0.1.2',
+    packages = find_packages(),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['Django>=1.3.1', 'South>=0.7.3'],
+
+    # Metadata for PyPI:
+    author = 'Jose Maria Zambrana Arze',
+    author_email = 'contact@josezambrana.com',
+    license = 'apache license v2.0',
+    url = 'http://github.com/mandlaweb/Zero-Common',
+    keywords = 'zero common app',
+    description = 'App with common views, libs and templates that can be used by other apps',
+    long_description = "An app with generic views, templates and styles to use in your project."
+
 )
